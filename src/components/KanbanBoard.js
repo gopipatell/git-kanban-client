4cde9a23dc31c20421b703c0cc9a47ba0af37707
Gopi Patel <ask2gopi@gmail.com>
2019-09-12 00:24:40 +1000

    drag and drop implemented

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -4,6 +4,7 @@ import serverURL from "../ServerURL";
 import './KanbanBoard.css'
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import KanbanList from './KanbanList.js'
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class KanbanBoard extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class KanbanBoard extends Component {
     };
 
     this._handleTaskSave = this._handleTaskSave.bind(this);
+    this._handleDragEnd = this._handleDragEnd.bind(this);
   }
 
   componentDidMount() {
@@ -30,27 +32,53 @@ class KanbanBoard extends Component {
 
     axios.post(serverURL("tasks"), payload)
       .then(result => {
-        this.setState({repository: { tasks: [ ...this.state.repository.tasks, result.data ] }})
+        this.setState({repository: { ...this.state.repository, tasks: [ ...this.state.repository.tasks, result.data ] }})
       });
   }
 
+  _handleDragEnd(result) {
+    console.log(result);
+
+    if(!result.destination) return;
+    console.log('nexttttt');
+    const {id} = this.props.match.params;
+
+    const payload = {
+      task_id: result.draggableId,
+      new_index: result.destination.index,
+      new_status: result.destination.droppableId
+    };
+
+    axios.post(serverURL(`repositories/${id}/update-task-index`), payload)
+    .then(result =>
+      axios.get(serverURL(`repositories/${id}`)).then((result) => {
+        this.setState({repository: result.data.repository});
+      }));
+
+  }
+
   render() {
-    const columns = ["Backlog", "Ready to start", "In progress", "Done", "Completed"];
+    const columns = ["Backlog", "Ready to start", "In progress", "Done"];
 
     return (
-      <div className="container-fluid mt-2">
+      <div className="container-fluid mt-2 kanban-board">
       <h4 className="text-capitalize">{this.state.repository.name.replace(/[-]/g,' ')}</h4>
-
+      <p>{this.state.repository.description}</p>
       <Row>
+      <DragDropContext onDragEnd={this._handleDragEnd}>
         { columns.map( (column, index) => {
             const status = ""+(index+1);
             return (
               <Col className="p-0" key={status}>
-                <KanbanList title={column} tasks={this.state.repository.tasks.filter(t => t.status === status)} handleTaskSave={ task => this._handleTaskSave({...task, status: status}) } />
+                <KanbanList
+                  title={column}
+                  tasks={this.state.repository.tasks.filter(t => t.status === status).sort((a,b)=>a.task_index-b.task_index)}
+                  handleTaskSave={ task => this._handleTaskSave({...task, status: status}) }
+                  status={status}/>
               </Col>
             )
         })}
-
+      </DragDropContext>
       </Row>
       </div>
     )
--- src/components/KanbanList.js
+++ src/components/KanbanList.js
@@ -2,6 +2,7 @@ import React, { Component }  from 'react';
 import './KanbanBoard.css'
 import { Card, Button } from 'react-bootstrap';
 import AddTaskModal from './AddTaskModal';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 class KanbanList extends Component {
 
@@ -27,24 +28,47 @@ class KanbanList extends Component {
     return (
       <div className="list">
         <div className="text-center">{this.props.title}</div>
-
-        {this.props.tasks.map(task => {
-          return(
-              <Card className="mt-1 shadow-sm" key={task.id} draggable>
-                <Card.Body>
-                  <Card.Text className="mb-0 text-capitalize">{task.title}</Card.Text>
-                  <Card.Text className="small">
-                    {task.description}
-                  </Card.Text>
-                  <Card.Link className="small text-secondary" href="#">Delete</Card.Link>
-                </Card.Body>
-              </Card>
-          )
-        })}
-
+        <Droppable droppableId={this.props.status+""}>
+        { provided => (
+          <div className="droppableList"
+            ref={provided.innerRef}
+            {...provided.droppableProps}>
+            {this.props.tasks.map(task => {
+              return(
+                <Draggable draggableId={task.id+""} index={task.task_index} key={task.id}>
+                {providedDrag => (
+                  <Card className="mt-1 shadow-sm"
+                    key={task.id}
+                    ref={providedDrag.innerRef}
+                    {...providedDrag.dragHandleProps}
+                    {...providedDrag.draggableProps}>
+                    <Card.Body>
+                      <Card.Text className="mb-0 text-capitalize">{task.title}</Card.Text>
+                      <Card.Text className="small">
+                        {task.description}
+                      </Card.Text>
+                      <Card.Link className="small text-secondary" href="#">Delete</Card.Link>
+                    </Card.Body>
+                  </Card>
+                )}
+                </Draggable>
+              )
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+        </Droppable>
         <br/>
-        <Button variant="light text-muted" size="sm" block onClick={this._handleShowModal}>+ Add new task</Button>
-        <AddTaskModal show={this.state.addTaskModalVisible}   handleClose={this._handleModalClose} handleTaskSave={ this.props.handleTaskSave}/>
+        <Button
+          variant="light text-muted"
+          size="sm" block
+          onClick={this._handleShowModal}>
+          + Add new task
+        </Button>
+        <AddTaskModal
+          show={this.state.addTaskModalVisible}
+          handleClose={this._handleModalClose}
+          handleTaskSave={ this.props.handleTaskSave} />
       </div>
     )
   }
