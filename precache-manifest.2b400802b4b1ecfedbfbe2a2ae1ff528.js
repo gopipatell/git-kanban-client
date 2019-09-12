edfd7136e0750236b6afd6312416ea5eb4abe8b9
Gopi Patel <ask2gopi@gmail.com>
2019-09-13 00:43:04 +1000

    Updates

---
+++ precache-manifest.2b400802b4b1ecfedbfbe2a2ae1ff528.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5510b43f6dc8d2a1702a8d4ef7a49fc",
+    "url": "/git-kanban-client/index.html"
+  },
+  {
+    "revision": "e89c729d6853e839bf63",
+    "url": "/git-kanban-client/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "5c0ab86bcff63dd8c781",
+    "url": "/git-kanban-client/static/css/main.5ca73d4e.chunk.css"
+  },
+  {
+    "revision": "e89c729d6853e839bf63",
+    "url": "/git-kanban-client/static/js/2.8c9ae45e.chunk.js"
+  },
+  {
+    "revision": "5c0ab86bcff63dd8c781",
+    "url": "/git-kanban-client/static/js/main.f9bc87f3.chunk.js"
+  },
+  {
+    "revision": "a8b8fcf6110ae33f912c",
+    "url": "/git-kanban-client/static/js/runtime~main.69ba39c2.js"
+  },
+  {
+    "revision": "acad9c9945aed203cbe2b02859c5e81e",
+    "url": "/git-kanban-client/static/media/kanban.acad9c99.jpg"
+  }
+]);
